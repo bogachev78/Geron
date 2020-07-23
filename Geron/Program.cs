3233548519a74dde66d519fe727638cb388547bf
Андрей Богачев <bogachev78@gmail.com>
2020-07-23 11:55:07 +0300

    Добавьте файлы проекта.

---
+++ Geron/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Geron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины трех сторон треугольника");
+            double one = Double.Parse(Console.ReadLine());
+            double two = Double.Parse(Console.ReadLine());
+            double three = Double.Parse(Console.ReadLine());
+
+            double p = 0.5 * (one + two + three);
+            double s = Math.Sqrt(p * (p - one) * (p - two) * (p - three));
+            Console.WriteLine($"Площадь трейгольни со сторонами {one}, {two}, {three} равна: {s}");
+        }
+    }
+}
